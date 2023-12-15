eefad024c3df68468bf9b513070be6a32c48b208
Honore <habizera16@gmail.com>
2023-12-15 03:26:35 -0800

    hello

---
+++ src/main/java/com/Emmanuel/Ecommerce/repository/RoleRepository.java
@@ -0,0 +1,7 @@
+package com.Emmanuel.Ecommerce.repository;
+
+import com.Emmanuel.Ecommerce.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+}
